9641edc52637f81aec05f9f2fcaba77f760096c4
Rowan Skewes <rowan.skewes@gmail.com>
2019-11-13 21:06:24 +1100

    Type-checking

--- src/Lambadas.purs
+++ src/Lambadas.purs
@@ -1,9 +1,15 @@
 module Lambadas where
 
 import Prelude
+
+import Data.Maybe (Maybe(..))
 import Effect.Console as Console
 import Effect.Unsafe (unsafePerformEffect)
 import Unsafe.Coerce (unsafeCoerce)
+
+
+------
+-- Types
 
 data ThingsOfInterest
   = Sun
@@ -11,21 +17,11 @@ data ThingsOfInterest
 
 derive instance eqThingOfInterest :: Eq ThingsOfInterest
 
-instance showInterestingThings :: Show ThingsOfInterest where
-  show thing = case thing of
-    Sun -> "\"Sun\""
-    Saturn -> "\"Saturn\""
-
 data Type
   = Atomic
   | Arrow Type Type
 
 derive instance eqType :: Eq Type
-
-instance showType :: Show Type where
-  show tau = case tau of
-    Atomic -> "Atomic"
-    Arrow source target -> show source <> " -> " <> show target
 
 data Term
   = Var Int
@@ -36,22 +32,20 @@ data Term
 
 derive instance eqTerm :: Eq Term
 
-instance showTerm :: Show Term where
-  show term = case term of
-    Var int -> show int
-    Application termHead termArg -> "(" <> show termHead <> ")(" <> show termArg <> ")"
-    Lambda term' -> "λ." <> show term'
-    Annotation term' tau -> show term' <> ": " <> show tau
-    ThingTerm thing -> show thing
+data Value
+  = ValClosure Closure
+  | Stuck StuckOnAVar
+  | ValThing ThingsOfInterest
+
+data StuckOnAVar -- neutral value
+  = StuckVar Int -- x_n
+  | StuckApplication StuckOnAVar Value
+
+data Closure = Closure EvaluationEnvironment Term
 
 data EvaluationEnvironment
   = Nil
   | Cons Value EvaluationEnvironment
-
-instance showEnv :: Show EvaluationEnvironment where
-  show env = case env of
-    Nil -> "[]"
-    Cons val moreEnv -> show moreEnv <> ", " <> show val
 
 envLength :: EvaluationEnvironment -> Int
 envLength env = case env of
@@ -67,15 +61,31 @@ envLookup env index = case env of
     then val
     else envLookup restEnv (index - 1)
 
-data Closure = Closure EvaluationEnvironment Term
+instance showInterestingThings :: Show ThingsOfInterest where
+  show thing = case thing of
+    Sun -> "\"Sun\""
+    Saturn -> "\"Saturn\""
+
+instance showType :: Show Type where
+  show tau = case tau of
+    Atomic -> "Atomic"
+    Arrow source target -> show source <> " -> " <> show target
+
+instance showTerm :: Show Term where
+  show term = case term of
+    Var int -> show int
+    Application termHead termArg -> "(" <> show termHead <> ")(" <> show termArg <> ")"
+    Lambda term' -> "λ." <> show term'
+    Annotation term' tau -> show term' <> ": " <> show tau
+    ThingTerm thing -> show thing
+
+instance showEnv :: Show EvaluationEnvironment where
+  show env = case env of
+    Nil -> "[]"
+    Cons val moreEnv -> show moreEnv <> ", " <> show val
 
 instance showClos :: Show Closure where
   show (Closure env term) = "Closure: [" <> show env <> "] " <> show term
-
-data Value
-  = ValClosure Closure
-  | Stuck StuckOnAVar
-  | ValThing ThingsOfInterest
 
 instance showValue :: Show Value where
   show val = case val of
@@ -83,14 +93,11 @@ instance showValue :: Show Value where
     Stuck stuckOnAVar -> "STUUUUUUUCK"
     ValThing thing -> show thing
 
-data StuckOnAVar
-  = StuckVar Int
-  | StuckApplication StuckOnAVar Term
 
-instantiateVar :: EvaluationEnvironment -> Value
-instantiateVar env =
-  Stuck (StuckVar (envLength env))
+------
+-- Evaluation
 
+-- apply synonymous with instantiate wtf?
 appClosure :: Closure -> Value -> Value
 appClosure (Closure env termBody) argument =
   let
@@ -98,22 +105,27 @@ appClosure (Closure env termBody) argument =
   in
     evaluate newEnv termBody
 
+appTerm :: Value -> Value -> Value
+appTerm valHead valArg =
+  case valHead of
+    ValClosure clos ->
+      appClosure clos valArg
+    Stuck stuckOnAVar ->
+      Stuck (StuckApplication stuckOnAVar valArg)
+    ValThing thing ->
+      unsafeCoerce (unsafePerformEffect (Console.log "that's a thing not a function!"))
+
 evaluate :: EvaluationEnvironment -> Term -> Value
 evaluate env term = case term of
   Var index ->
     envLookup env index
 
   Application termHead termArgument ->
-    case evaluate env termHead of
-      ValClosure clos ->
-        let
-          valArg = evaluate env termArgument
-        in
-          appClosure clos valArg
-      Stuck stuckOnAVar ->
-        Stuck (StuckApplication stuckOnAVar termArgument)
-      ValThing thing ->
-        unsafeCoerce (unsafePerformEffect (Console.log "that's a thing not a function!"))
+    let
+      valHead = evaluate env termHead
+      valArg = evaluate env termArgument
+    in
+      appTerm valHead valArg
 
   Lambda termBody ->
     ValClosure (Closure env termBody)
@@ -122,3 +134,58 @@ evaluate env term = case term of
     evaluate env termA
 
   ThingTerm thing -> ValThing thing
+
+
+------
+-- Type-checking
+
+data Context
+  = EmptyContext
+  | GammaCons Type Context
+
+contextLookup :: Context -> Int -> Maybe Type
+contextLookup context pos =
+  case context of
+    EmptyContext ->
+      Nothing
+    GammaCons t nextContext ->
+      if pos == 0
+      then
+        Just t
+      else
+        contextLookup nextContext (pos - 1)
+
+synth :: Context -> Term -> Maybe Type
+synth context term =
+  case term of
+    Var int ->
+      contextLookup context int
+    Application termHead termArg ->
+      case synth context termHead of
+        Just (Arrow paramType bodyType) ->
+          if check context termArg paramType
+          then Just bodyType
+          else Nothing
+        _ ->
+          Nothing
+    Lambda body ->
+      Nothing
+    Annotation term' t ->
+      if check context term' t
+      then Just t
+      else Nothing
+    ThingTerm something ->
+      Just Atomic
+
+check :: Context -> Term -> Type -> Boolean
+check context term t =
+  case term, t of
+    Lambda body, Arrow paramType bodyType ->
+      let
+        bodyContext = GammaCons paramType context
+      in
+        check bodyContext body bodyType
+    _, _ ->
+      case synth context term of
+        Just t' -> t == t'
+        Nothing -> false
--- src/Main.purs
+++ src/Main.purs
@@ -3,15 +3,19 @@ module Main where
 import Prelude
 
 import Effect (Effect)
-import Effect.Console (log)
-import Lambadas (evaluate, EvaluationEnvironment(..), Value(..), ThingsOfInterest(..), Term(..))
+import Effect.Console (log, logShow)
+import Lambadas (Context(..), EvaluationEnvironment(..), Term(..), ThingsOfInterest(..), Type(..), Value(..), check, evaluate)
+
 
 main :: Effect Unit
 main =
   let
     environment = Cons (ValThing Sun) Nil
     id = Lambda (Var 0)
-    term = Application id id
+    term = Application (Annotation id (Arrow (Arrow Atomic Atomic) (Arrow Atomic Atomic))) id
     evaluatedTerm = evaluate environment term
-  in
+  in do
     log (show evaluatedTerm)
+    logShow $ check EmptyContext id (Arrow Atomic Atomic)
+    logShow $ check EmptyContext id (Arrow (Arrow Atomic Atomic) (Arrow Atomic Atomic))
+    logShow $ check EmptyContext term (Arrow Atomic Atomic)
